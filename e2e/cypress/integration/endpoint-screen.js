584579a5256ee3a1a9b881d93425c79efa0d5220
botre <git@bjornkrols.com>
2022-05-27 07:11:41 +0200

    Endpoint screen tests: new id before each test

--- e2e/cypress/integration/endpoint-screen.js
+++ e2e/cypress/integration/endpoint-screen.js
@@ -1,15 +1,19 @@
-const TEST_ID = Math.random().toString(36).slice(2, 7);
-const TEST_ENDPOINT_PATH = `/to/${TEST_ID}`;
-const TEST_ENDPOINT_URL = `${location.protocol}//${location.host}${TEST_ENDPOINT_PATH}`;
-
 describe("Endpoint screen", () => {
+  let testId = "";
+  let testEndpointPath = "";
+  let testEndpointUrl = "";
+
   beforeEach(() => {
-    cy.visit(`/${TEST_ID}`);
+    testId = Math.random().toString(36).slice(2, 7);
+    testEndpointPath = `/to/${testId}`;
+    testEndpointUrl = `${location.protocol}//${location.host}${testEndpointPath}`;
+
+    cy.visit(`/${testId}`);
   });
 
   describe("Title", () => {
     it("should be correct", () => {
-      cy.title().should("eq", `${TEST_ID} | go-project`);
+      cy.title().should("eq", `${testId} | go-project`);
     });
   });
 
@@ -18,7 +22,7 @@ describe("Endpoint screen", () => {
       cy.location().then((location) => {
         cy.get('[data-test="unique-endpoint-url').should(
           "contain.text",
-          TEST_ENDPOINT_URL
+          testEndpointUrl
         );
       });
     });
@@ -33,19 +37,17 @@ describe("Endpoint screen", () => {
     });
 
     it("should not show a waiting message if some requests are found", () => {
-      cy.exec(`curl -X POST -d 'Hello World!' ${TEST_ENDPOINT_URL}`).then(
-        () => {
-          cy.get('[data-test="requests').should(
-            "not.contain.text",
-            "Waiting for requests..."
-          );
-        }
-      );
+      cy.exec(`curl -X POST -d 'Hello World!' ${testEndpointUrl}`).then(() => {
+        cy.get('[data-test="requests').should(
+          "not.contain.text",
+          "Waiting for requests..."
+        );
+      });
     });
 
     it("should display new requests in real-time", () => {
       const requestBody = "Hello World!";
-      cy.exec(`curl -X POST -d '${requestBody}' ${TEST_ENDPOINT_URL}`).then(
+      cy.exec(`curl -X POST -d '${requestBody}' ${testEndpointUrl}`).then(
         () => {
           cy.get('[data-test="requests').should("contain.text", requestBody);
         }
@@ -53,27 +55,23 @@ describe("Endpoint screen", () => {
     });
 
     it("should display the request details", () => {
-      cy.exec(`curl -X POST -d 'Hello World!' ${TEST_ENDPOINT_URL}`).then(
-        () => {
-          cy.get('[data-test="request-details').contains(/now|seconds? ago/);
-          cy.get('[data-test="request-details').contains("127.0.0.1");
-          cy.get('[data-test="request-details').contains("POST");
-          cy.get('[data-test="request-details').contains(TEST_ENDPOINT_PATH);
-        }
-      );
+      cy.exec(`curl -X POST -d 'Hello World!' ${testEndpointUrl}`).then(() => {
+        cy.get('[data-test="request-details').contains(/now|seconds? ago/);
+        cy.get('[data-test="request-details').contains("127.0.0.1");
+        cy.get('[data-test="request-details').contains("POST");
+        cy.get('[data-test="request-details').contains(testEndpointPath);
+      });
     });
 
     it("should display the request headers", () => {
-      cy.exec(`curl -X POST -d 'Hello World!' ${TEST_ENDPOINT_URL}`).then(
-        () => {
-          cy.get('[data-test="request-headers').contains("Content-Type");
-        }
-      );
+      cy.exec(`curl -X POST -d 'Hello World!' ${testEndpointUrl}`).then(() => {
+        cy.get('[data-test="request-headers').contains("Content-Type");
+      });
     });
 
     it("should display the request body", () => {
       const requestBody = "Hello World!";
-      cy.exec(`curl -X POST -d '${requestBody}' ${TEST_ENDPOINT_URL}`).then(
+      cy.exec(`curl -X POST -d '${requestBody}' ${testEndpointUrl}`).then(
         () => {
           cy.get('[data-test="request-body').contains(requestBody);
         }
@@ -82,7 +80,7 @@ describe("Endpoint screen", () => {
 
     it("should be possible to filter based on the request body", () => {
       const requestBody = "Hello World!";
-      cy.exec(`curl -X POST -d '${requestBody}' ${TEST_ENDPOINT_URL}`).then(
+      cy.exec(`curl -X POST -d '${requestBody}' ${testEndpointUrl}`).then(
         () => {
           cy.get('[data-test="requests').should("contain.text", requestBody);
 
@@ -103,7 +101,7 @@ describe("Endpoint screen", () => {
       const requestHeaderValue = "Hello World!";
 
       cy.exec(
-        `curl -X POST -H '${requestHeaderKey}: ${requestHeaderValue}' ${TEST_ENDPOINT_URL}`
+        `curl -X POST -H '${requestHeaderKey}: ${requestHeaderValue}' ${testEndpointUrl}`
       ).then(() => {
         cy.get('[data-test="requests').should("contain.text", requestHeaderKey);
         cy.get('[data-test="requests').should(
