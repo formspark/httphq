a428c1c0ed2be3b373d07fb21df3bb26bda0e475
botre <git@bjornkrols.com>
2022-06-04 12:43:54 +0200

    Upgrade to Cypress 10

---
+++ e2e/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:8080",
+    supportFile: false,
+  },
+});
